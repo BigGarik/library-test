d32a897145dd76247ba219c9fc5e06e38b9fbba6
big <inetsmol@gmail.com>
2025-06-04 12:31:53 +0300

    тесты бизнес логики

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -8,15 +8,20 @@ client = TestClient(app)
 ALGORITHM = "HS256"
 
 
-# Тест для проверки доступа с валидным токеном
 def test_get_readers_with_token():
+    """
+    Тест для проверки доступа с валидным токеном
+    """
     data = {"sub": "1"}
     token = create_access_token(data)
     response = client.get("/readers", headers={"Authorization": f"Bearer {token}"})
     assert response.status_code == 200
 
 
-# Тест для проверки доступа без токена
+
 def test_get_readers_without_token():
+    """
+    Тест для проверки доступа без токена
+    """
     response = client.get("/readers")
     assert response.status_code == 401
